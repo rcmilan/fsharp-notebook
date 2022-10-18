6bce251672bf471dc8f59aa38b855aa760d2f029
Ricardo Milan <ricardo@fluencypass.com>
2022-10-18 15:00:54 -0300

    typo

--- scripts/conditional.fsx
+++ scripts/conditional.fsx
@@ -2,10 +2,10 @@ module ConditionalModule
     let checkGreater (x : int , y : int) =
         if x = y then "equal"
         elif x < y then $"{x} is less than {y}"
-        else $"{x} is greather than {y}"
+        else $"{x} is greater than {y}"
 
     let matchGreater (x : int , y : int) =
         match x with
         | x when x = y -> "equal"
         | x when x < y -> $"{x} is less than {y}"
-        | _ ->  $"{x} is greather than {y}"
+        | _ ->  $"{x} is greater than {y}"
