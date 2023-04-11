463d8509ac3c173971761ac0a5bb8ce023e888ea
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-04-11 18:30:07 -0300

    recursive

--- scripts/functions.fsx
+++ scripts/functions.fsx
@@ -4,3 +4,8 @@ module FunctionModule
 
     let memoizationSum z =
         calculateSum z 10
+
+    let rec recursiveSum x =
+        match x with
+        | 0 | 100 -> x
+        | n -> calculateSum x 1 |> recursiveSum
